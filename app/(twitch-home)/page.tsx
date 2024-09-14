f1d090edae0f5b5715572c612844aee01eeb2a39
Srinjoy Barman <srinjoy.barman@pwc.com>
2024-09-14 20:42:06 +0530

    delete proj

--- app/(twitch-home)/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import twitchJson from "@/public/twitch-icon.json";
-import Lottie, { LottieRef } from "lottie-react";
-import { useRef } from "react";
-
-export default function Home() {
-  return <div>home</div>;
-}
